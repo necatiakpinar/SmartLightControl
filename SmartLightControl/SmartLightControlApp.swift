92d35bf02bc0da4266399e10e37ec81efc935757
Necati Akpinar <necatiakpinar1997@gmail.com>
2023-11-18 09:36:11 +0300

    Initial Commit

---
+++ SmartLightControl/SmartLightControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartLightControlApp.swift
+//  SmartLightControl
+//
+//  Created by Necati Akpınar on 18.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SmartLightControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
